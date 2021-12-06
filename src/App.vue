5a2d041ffa81bd813feb565bd076c62602b72779
unknown <sdlbsso@gmail.com>
2021-12-06 14:39:08 -0300

    aumenté cantidad de registros a mostrar

--- src/App.vue
+++ src/App.vue
@@ -56,7 +56,7 @@ export default {
   },
   async mounted() {
     const res = await fetch(
-      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false"
+      "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=30&page=1&sparkline=false"
     );
     const data = await res.json();
     this.coins = data;
